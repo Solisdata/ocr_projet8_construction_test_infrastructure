63809ec6fecf2a2414defd3fc27911ecf233cb0f
Solisdata <mathilde.lesolliec@gmail.com>
2026-01-23 16:48:36 +0100

    add tests_integration

--- load/tests_integration.py
+++ load/tests_integration.py
@@ -1,10 +1,26 @@
-import pytest
-from datetime import datetime
-from load.load_mongo import (
-    clean_dataframe,
-    connect_mongo,
-    get_latest_s3_key,
-    load_s3_jsonl,
-    BUCKET_NAME,
-    df1_infoclimat_prefix
-)
+import time
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb://localhost:27017/"  # Docker local
+DB_NAME = "meteo_db"
+COLLECTION_NAME = "stations_hourly"
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+def measure_query_time(query_func, description=""):
+    start = time.time()
+    result = query_func()
+    end = time.time()
+    print(f"{description} : {end-start:.4f}s, {len(list(result))} documents")
+    return end-start
+
+# 1. Toutes les stations
+measure_query_time(lambda: collection.find({}, {"id": 1}), "Toutes les stations")
+
+# 2. Hourly d’une station
+measure_query_time(lambda: collection.find({"id": "ILAMAD25"}, {"hourly": 1}), "Hourly station ILAMAD25")
+
+# 3. Filtrage par date (ex: dernière mesure)
+measure_query_time(lambda: collection.find({"hourly.dh_utc": {"$gte": "2026-01-23T12:00:00"}}), "Filtre par date")
